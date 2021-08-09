a78cebbb9fcb31ce1a29279e08a14a5b104cab37
prowinter <prowinter@aliyun.com>
2021-08-09 23:59:23 +0800

    STM32_Printf

---
+++ STM32_Printf.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+PUTCHAR_PROTOTYPE
+{
+    // HAL库函数版本
+    // HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1 , 0xffff);
+    // return ch;
+
+    // 寄存器版本
+    while ((USART1->SR & 0X40) == 0);
+    USART1->DR = (uint8_t) ch;
+    return ch;
+}
+
+// VScode 需额外添加此函数
+// 若需使用Printf,Sprintf操作Float型变量，还需在Makefile 对进行此操作 LDFLAGS = $(MCU) -u_printf_float -u_sprintf_float
+int _write(int file, char *ptr,int len)
+{
+  int DataIdx;
+  for (DataIdx=0;DataIdx<len;DataIdx++)
+  {
+    __io_putchar(*ptr++);
+  }
+  return len;
+}
